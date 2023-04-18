44166aa0c55609979f4cd411bc242b44eee9bef8
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2023-04-18 21:21:41 +0500

    react npm and react-icon and siteNavbar

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import logo from './1111111-removebg-preview.png'
+
+export {logo}
---
+++ src/util/constants.js
@@ -0,0 +1,21 @@
+export const navigationLinks = [
+  {
+    id:'home',
+    title:'Bosh sahifa',
+  },
+
+  {
+    id:'features',
+    title:'Xizmatlar',
+  },
+
+  {
+    id:'praduct',
+    title:'Mahsulotlar',
+  },
+
+  {
+    id:'clients',
+    title:'Mijozlar',
+  },
+]
